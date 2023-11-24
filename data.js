194847ec164f102d67bcec731b8bb11c051a1397
Ayush-Manware <ayushmanware19@gmail.com>
2023-11-24 12:18:39 +0530

    Middleware Completed

---
+++ data.js
@@ -0,0 +1,36 @@
+const data1 = {
+    heading:"Data 1",
+    name:"Ayush",
+    age:22,
+    course:"MERN"
+}
+
+const data2 = {
+    heading:"Data 2",
+    name:"Prateek",
+    age:22,
+    course:"MERN"
+}
+
+const data3 = {
+    heading:"Data 3",
+    name:"Piyush",
+    age:22,
+    course:"MERN"
+}
+
+const data4 = {
+    heading:"Data 4",
+    name:"Suraj",
+    age:22,
+    course:"MERN"
+}
+
+const data5 = {
+    heading:"Data 5",
+    name:"Aman",
+    age:22,
+    course:"MERN"
+}
+
+module.exports = {data1, data2, data3, data4, data5}
